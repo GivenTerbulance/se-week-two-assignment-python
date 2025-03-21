75e5f1aae1ceb40e8e4abe688e0423f1937741ec
GivenTerbulance <teboho4000@gmail.com>
2025-03-21 16:20:48 +0200

    Python task2

---
+++ task2.py
@@ -0,0 +1,22 @@
+#Append
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#Insert
+my_list.insert(1, 15)
+#Extend
+her_list = [50, 60, 70]
+my_list.extend(her_list)
+#Remove
+my_list.remove(70)
+#Sort
+my_list.sort()
+#Index of the value 30
+index = my_list.index(30)
+print(index)
+#Final Execution 
+print(my_list)
+
+
